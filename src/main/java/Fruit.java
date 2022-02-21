19a24c3c6f486105ca91cdc47dd7b47cdf986e2d
kirya.kuzmich <kirya.kuzmich@gmail.com>
2022-02-21 18:34:40 +0200

    less5

---
+++ src/main/java/Fruit.java
@@ -0,0 +1,11 @@
+public class Fruit {
+    float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        numbaOne();
+        numbaTwo();
+        numbaThree();
+
+    }
+
+    public static void numbaOne() {
+
+        Integer[] iArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        Double[] dArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0};
+        String[] sArray = {" one ", " two ", " three ", " four ", " five ", " six ", " seven "};
+        try {
+            swapTwoElementsInArray(iArray, 0, 2);
+            swapTwoElementsInArray(dArray, 0, 2);
+            swapTwoElementsInArray(sArray, 0, 2);
+        } catch (WrongPositionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Swap result in integer array: " + Arrays.toString(iArray));
+        System.out.println("Swap result in double array: " + Arrays.toString(dArray));
+        System.out.println("Swap result in string array: " + Arrays.toString(sArray));
+    }
+
+    public static void numbaTwo() {
+        Integer[] iArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        Double[] dArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0};
+        String[] sArray = {"one", "two", "three", "four", "five"};
+        arrayToArrayList(iArray);
+        arrayToArrayList(dArray);
+        arrayToArrayList(sArray);
+        System.out.println(Arrays.toString(iArray));
+        System.out.println(Arrays.toString(dArray));
+        System.out.println(Arrays.toString(sArray));
+    }
+
+    public static void numbaThree() {
+        Box<Apple> boxWithApple = new Box<>(new Apple(), new Apple(), new Apple()); //picking apples in box
+        Box<Orange> boxWithOranges = new Box<>(new Orange(), new Orange()); //picking apples in box
+        Box<Orange> anotherBoxWithOranges = new Box<>();
+        System.out.println("A box of apples weighs " + boxWithApple.getWeight()); //weight box with apples
+        System.out.println("A box of oranges weight " + boxWithOranges.getWeight()); //weight box with apples
+        System.out.println(boxWithApple.compare(boxWithOranges));
+        boxWithOranges.replaceFruitsToAnotherBox(anotherBoxWithOranges);
+        System.out.println(boxWithOranges.getBox());
+        System.out.println(anotherBoxWithOranges.getBox());
+    }
+
+    private static void swapTwoNumbersInArray(int[] array, int firstNumberPos, int secondNumberPos) throws WrongPositionException {
+        if (firstNumberPos < 0 || firstNumberPos > array.length ||
+                secondNumberPos < 0 || secondNumberPos > array.length || firstNumberPos == secondNumberPos) {
+            throw new WrongPositionException("Неправильно указаны позиции элементов для замены");
+        }
+        array[firstNumberPos] ^= array[secondNumberPos];
+        array[secondNumberPos] ^= array[firstNumberPos];
+        array[firstNumberPos] ^= array[secondNumberPos];
+    }
+
+    private static void swapTwoElementsInArray(Object[] array, int firstNumPos, int secondNumPos) throws WrongPositionException {
+        if (firstNumPos < 0 || firstNumPos > array.length ||
+                secondNumPos < 0 || secondNumPos > array.length || firstNumPos == secondNumPos) {
+            throw new WrongPositionException("Неправильно указаны позиции элементов для замены");
+        }
+        Object temp = array[firstNumPos];
+        array[firstNumPos] = array[secondNumPos];
+        array[secondNumPos] = temp;
+    }
+
+    private static <T> ArrayList arrayToArrayList(T[] array) {
+        return new ArrayList<>(Arrays.asList(array));
+    }
+}
